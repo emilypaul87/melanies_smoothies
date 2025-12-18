6474c135e149e483d11fe59dd2200a2373994493
emilypaul87 <emilypaul87@gmail.com>
2025-12-19 00:25:13 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-# Import python packages
+RCH_ON# Import python packages
 import streamlit as st
 import requests
 from snowflake.snowpark.functions import col
@@ -14,7 +14,7 @@ st.write("The name on your smoothie will be:", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col(search_on))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col(SEARCH_ON))
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
